ef40251fc74721223bcdddf7d41476ec9a4a61db
evan-kapantais <evankapantais@gmail.com>
2022-06-19 17:16:00 +0300

    restyle, js almost done

--- main.js
+++ main.js
@@ -1,73 +1,180 @@
-const alphabet = [
-	'a',
-	'b',
-	'c',
-	'd',
-	'e',
-	'f',
-	'g',
-	'h',
-	'i',
-	'j',
-	'k',
-	'l',
-	'm',
-	'n',
-	'o',
-	'p',
-	'q',
-	'r',
-	's',
-	't',
-	'u',
-	'v',
-	'w',
-	'x',
-	'y',
-	'z',
-];
+// TODO: download strings
+
+import alphabet from './alphabet.js';
 
 const copyButton = document.querySelector('.copy-button');
-const form = document.querySelector('form');
+const saveButton = document.querySelector('.save-button');
+const downloadButton = document.querySelector('.download-button');
+
+const textInput = document.querySelector('#text');
+const controlsForm = document.querySelector('.controls-form');
+
+const controls = {};
+const strings = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-	form.addEventListener('submit', handleFormSubmit);
+	checkLocalStorage();
+	controlsForm.addEventListener('change', handleValueChange);
+	textInput.addEventListener('input', handleValueChange);
 	copyButton.addEventListener('click', copyToClipboard);
+	saveButton.addEventListener('click', saveString);
+	downloadButton.addEventListener('click', downloadSavedStrings);
 });
+
+// TODO: Remove in production
+function l(text) {
+	console.log(text);
+}
+
+function setControls(options) {
+	if (!options) {
+		controls.algo = document.querySelector('[name="algo"]').value;
+		controls.direction = document.querySelector('[name="direction"]').checked
+			? 'backward'
+			: 'forward';
+		controls.trim = document.querySelector('[name="trim"]').checked
+			? true
+			: false;
+	}
+}
+
+function checkLocalStorage() {
+	const localItems = JSON.parse(localStorage.getItem('rotside'));
+	const localStrings = JSON.parse(localStorage.getItem('rotside_strings'));
+
+	if (!localItems) {
+		setControls();
+		localStorage.setItem('rotside', JSON.stringify(controls));
+		return;
+	}
+
+	const { algo, direction, trim } = localItems;
+
+	document.querySelector('[name="algo"]').value = algo;
+	document.querySelector(
+		`[name="direction"][value="${direction}"]`
+	).checked = true;
+	document.querySelector(`[name="trim"][value="${trim}"]`).checked = true;
+
+	const directionIcon = document.querySelector('.direction-icon');
+
+	if (direction === 'backward') {
+		directionIcon.classList.add('backward');
+	} else {
+		directionIcon.classList.remove('backward');
+	}
+
+	if (!localStrings) {
+		localStorage.setItem('rotside_strings', JSON.stringify([]));
+		return;
+	}
+
+	strings.push(...localStrings);
+
+	const stringsList = document.querySelector('.strings-list ul');
+
+	strings.forEach((string) => {
+		const li = document.createElement('li');
+		li.textContent = string;
+		stringsList.appendChild(li);
+	});
+}
+
+function showOutputMessage(type) {
+	const output = document.querySelector('.output');
+	const className = type == 'copied' ? 'copied' : 'saved';
+
+	output.classList.add(className);
+
+	setTimeout(() => {
+		output.classList.remove(className);
+	}, 4000);
+}
 
 function copyToClipboard() {
 	const output = document.querySelector('.output');
 
 	navigator.clipboard.writeText(output.textContent);
-	output.classList.add('active');
 
-	setTimeout(() => {
-		output.classList.remove('active');
-	}, 4000);
+	showOutputMessage('copied');
 }
 
-function handleFormSubmit(e) {
-	e.preventDefault();
+function saveString() {
+	const text = document.querySelector('.output').textContent;
 
-	const input = e.target.querySelector('#text').value;
-	const algo = e.target.querySelector('#algo').value;
-	const type = e.target.querySelector('#type').value;
-	const trim = e.target.querySelector('#trim').checked;
+	const strings = JSON.parse(localStorage.getItem('rotside_strings'));
+	strings.push(text);
+
+	localStorage.setItem('rotside_strings', JSON.stringify(strings));
+
+	const stringsList = document.querySelector('.strings-list ul');
+
+	const li = document.createElement('li');
+	li.textContent = text;
+	stringsList.appendChild(li);
+
+	showOutputMessage('saved');
+}
+
+function downloadSavedStrings() {
 	const output = document.querySelector('.output');
+	const text = output.textContent;
 
-	const places = Number(algo.substr(3));
+	const link = document.createElement('a');
+	link.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(text);
+	link.download = 'rotside.txt';
+	link.click();
+}
+
+function handleValueChange() {
+	const text = document.querySelector('#text').value;
+
+	const output = document.querySelector('.output');
+	const directionIcon = document.querySelector('.direction-icon');
+
+	const algo = document.querySelector('[name="algo"]').value;
+	const direction = document.querySelector('[name="direction"]').checked
+		? 'backward'
+		: 'forward';
+	const trim = document.querySelector('[name="trim"]').checked ? true : false;
+
+	if (direction === 'backward') {
+		directionIcon.classList.add('backward');
+	} else {
+		directionIcon.classList.remove('backward');
+	}
+
+	// Update local items
+	controls.algo = algo;
+	controls.direction = direction;
+	controls.trim = trim;
+
+	localStorage.setItem('rotside', JSON.stringify(controls));
 
 	const rotated =
-		type === 'forwards'
-			? rotateStringForwards(input, places, trim)
-			: rotateStringBackwards(input, places, trim);
+		direction === 'forward'
+			? rotateStringForwards(text, parseInt(algo.substr(3)), trim)
+			: rotateStringBackwards(text, parseInt(algo.substr(3)), trim);
 
-	output.textContent = rotated;
+	if (text) {
+		copyButton.classList.add('shown');
+		saveButton.classList.add('shown');
+	} else {
+		copyButton.classList.remove('shown');
+		saveButton.classList.remove('shown');
+	}
+	// text
 
-	copyButton.classList.add('shown');
+	if (rotated) {
+		output.classList.remove('placeholder');
+		output.textContent = rotated;
+	} else {
+		output.classList.add('placeholder');
+		output.textContent = 'Your output here';
+	}
 }
 
-function rotateStringForwards(text, places, trim = false) {
+function rotateStringForwards(text = '', places = 0, trim = false) {
 	const arr = trim ? text.split(' ').join('').split('') : text.split('');
 
 	let output = '';
@@ -106,7 +213,7 @@ function rotateStringForwards(text, places, trim = false) {
 	return output;
 }
 
-function rotateStringBackwards(text, places, trim = false) {
+function rotateStringBackwards(text = '', places = 0, trim = false) {
 	const arr = trim ? text.split(' ').join('').split('') : text.split('');
 
 	let output = '';
